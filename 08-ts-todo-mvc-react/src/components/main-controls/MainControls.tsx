d6342a859105daae650fd4774c35015cc80f0b5a
IlyaAAAA <ilya-sachuk@mail.ru>
2021-03-12 00:07:18 +0300

    deleted comments and autofocus

--- 08-ts-todo-mvc-react/src/components/main-controls/MainControls.tsx
+++ 08-ts-todo-mvc-react/src/components/main-controls/MainControls.tsx
@@ -1,16 +1,12 @@
-// HOME WORK COMPONENT! Please don't rewrite me during lecture!
 import React, { createRef } from 'react';
 
-// HOME WORK COMPONENT! Please don't rewrite me during lecture!
 export class MainControls extends React.Component<{
     addNewTodo: (text: string) => void,
-    markAllAsReady: () => void
+    markAllAsReady: () => void,
 }> {
   _inputRef: React.RefObject<HTMLInputElement> = createRef();
 
-  // HOME WORK COMPONENT! Please don't rewrite me during lecture!
   render() {
-    // HOME WORK COMPONENT! Please don't rewrite me during lecture!
     const onSubmit = (e: React.FormEvent) => {
       e.preventDefault();
       const input: HTMLInputElement = this._inputRef.current as HTMLInputElement;
@@ -21,7 +17,6 @@ export class MainControls extends React.Component<{
       }
     }
 
-    // HOME WORK COMPONENT! Please don't rewrite me during lecture!
     return (
       <section className="todo-app__main-controls main-controls">
         <div className="main-controls__select-all">
@@ -40,7 +35,7 @@ export class MainControls extends React.Component<{
             className="main-controls__create-new-input"
             placeholder="What needs to be done?"
             aria-label="Add new item"
-            // autoFocus="autofocus"
+            autoFocus
           />
         </form>
       </section>
