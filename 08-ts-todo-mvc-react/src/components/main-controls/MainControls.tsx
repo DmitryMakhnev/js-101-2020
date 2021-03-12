b0aa8584167a29ad815ca17b6fd5d122e18d569c
Maxim-Byte <maxim5gromov@gmail.com>
2021-03-12 16:30:31 +0300

    Main component from js to ts

--- 08-ts-todo-mvc-react/src/components/main-controls/MainControls.tsx
+++ 08-ts-todo-mvc-react/src/components/main-controls/MainControls.tsx
@@ -1,4 +1,3 @@
-// HOME WORK COMPONENT! Please don't rewrite me during lecture!
 import React, { createRef } from 'react';
 
 // HOME WORK COMPONENT! Please don't rewrite me during lecture!
@@ -8,17 +7,16 @@ export class MainControls extends React.Component {
   // HOME WORK COMPONENT! Please don't rewrite me during lecture!
   render() {
     // HOME WORK COMPONENT! Please don't rewrite me during lecture!
-    const onSubmit = e => {
+    const onSubmit = e : React.FormEvent<HTMLFormElement> => {
       e.preventDefault();
-      const input = this._inputRef.current;
-      const currentNewTodoText = input.value.trim();
+      const input : HTMLInputElement = this._inputRef.current;
+      const currentNewTodoText : string = input.value.trim();
       if (currentNewTodoText) {
-        input.value = '';
+        input.value  = '';
         this.props.addNewTodo(currentNewTodoText);
       }
     }
 
-    // HOME WORK COMPONENT! Please don't rewrite me during lecture!
     return (
       <section className="todo-app__main-controls main-controls">
         <div className="main-controls__select-all">
