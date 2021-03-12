eb2f341501872ce607f1b3b46c63d357e9386c36
Dmitry Makhnev <dmitry.makhnev@jetbrains.com>
2021-03-12 12:13:29 +0300

    fix(test:e2e): add interceptor

--- 09-ts-todo-mvc-react-with-tests/tests/e2e/app.spec.ts
+++ 09-ts-todo-mvc-react-with-tests/tests/e2e/app.spec.ts
@@ -1,7 +1,21 @@
 
 describe('App', () => {
   it('Should display auth page by default', function () {
-    cy.visit('http://localhost:3000')
-    cy.get('.auth-page').should('be.visible')
+    cy.visit('http://localhost:3000');
+    cy.get('.auth-page').should('be.visible');
   });
+
+  it(`Shouldn't be display for authorized user`, () => {
+    cy.intercept(
+      'GET',
+      'http://localhost:3012/user',
+      { fixture: 'user.response.json' }
+    ).as('user');
+
+    cy.visit('http://localhost:3000')
+      .wait('@user');
+
+    cy.get('.auth-page').should('not.exist');
+  });
+
 })
