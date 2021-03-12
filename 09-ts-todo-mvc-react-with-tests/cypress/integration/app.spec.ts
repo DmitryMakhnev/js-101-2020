11ad59ef835ae2b27478bb10b6abb8535754798e
Dmitry Makhnev <dmitry.makhnev@jetbrains.com>
2021-03-12 11:46:14 +0300

    fix: cypress tests

--- 09-ts-todo-mvc-react-with-tests/cypress/integration/app.spec.ts
+++ 09-ts-todo-mvc-react-with-tests/cypress/integration/app.spec.ts
@@ -1,6 +1,6 @@
 
-describe('CRA', () => {
-  it('shows learn link', function () {
+describe('App', () => {
+  it('Should display auth page by default', function () {
     cy.visit('http://localhost:3000')
     cy.get('.auth-page').should('be.visible')
   });
