23746cf83baca24e2422cf32324111a632c62b28
pa-artem <pa.artem20@yandex.ru>
2021-03-22 22:42:08 +0300

    Add TS types to "main controls" component

--- 08-ts-todo-mvc-react/src/components/main-controls/MainControls.tsx
+++ 08-ts-todo-mvc-react/src/components/main-controls/MainControls.tsx
@@ -1,24 +1,22 @@
-// HOME WORK COMPONENT! Please don't rewrite me during lecture!
 import React, { createRef } from 'react';
 
-// HOME WORK COMPONENT! Please don't rewrite me during lecture!
-export class MainControls extends React.Component {
-  _inputRef = createRef();
+export class MainControls extends React.Component<{
+  addNewTodo: (text: string) => void;
+  markAllAsReady: () => void;
+}> {
+  _inputRef = createRef<HTMLInputElement>();
 
-  // HOME WORK COMPONENT! Please don't rewrite me during lecture!
   render() {
-    // HOME WORK COMPONENT! Please don't rewrite me during lecture!
-    const onSubmit = e => {
+    const onSubmit: React.FormEventHandler<HTMLFormElement> = e => {
       e.preventDefault();
-      const input = this._inputRef.current;
-      const currentNewTodoText = input.value.trim();
+      const input: HTMLInputElement = this._inputRef.current as HTMLInputElement;
+      const currentNewTodoText: string = input.value.trim();
       if (currentNewTodoText) {
         input.value = '';
         this.props.addNewTodo(currentNewTodoText);
       }
-    }
+    };
 
-    // HOME WORK COMPONENT! Please don't rewrite me during lecture!
     return (
       <section className="todo-app__main-controls main-controls">
         <div className="main-controls__select-all">
@@ -37,7 +35,7 @@ export class MainControls extends React.Component {
             className="main-controls__create-new-input"
             placeholder="What needs to be done?"
             aria-label="Add new item"
-            autoFocus="autofocus"
+            autoFocus={true}
           />
         </form>
       </section>
