3c3c46787cd0fb08f146bc66075f37594d12fc61
OGSegu <seguisreal@gmail.com>
2021-05-15 22:49:26 +0300

    HW2

--- 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
+++ 09-ts-todo-mvc-react-with-tests/tests/e2e/todos-page/manage-todos.spec.ts
@@ -1,6 +1,6 @@
 import { todosEndpoint, getUserEndpoint, pageUrl } from '../../../src/consts/urls';
 
-function authorize() {
+function authorize(fixture: string) {
   cy.intercept(
     'GET',
     getUserEndpoint,
@@ -10,7 +10,7 @@ function authorize() {
   cy.intercept(
     'GET',
     todosEndpoint,
-    { fixture: 'empty-todos-list.response.json' }
+    { fixture: fixture }
   ).as('initialTodos');
 
   cy.visit(pageUrl)
@@ -40,7 +40,7 @@ describe('Manage Todos', () => {
 
       const itemText = Math.random().toString() + '_' + Date.now();
 
-      authorize();
+      authorize('empty-todos-list.response.json');
       cy.get('[data-test-id=create-new-todo-form]').should('be.visible');
       cy.get('[data-test-id=create-new-todo-form__todo-text-input]').type(itemText);
       cy.get('[data-test-id=create-new-todo-form]').submit();
@@ -53,6 +53,35 @@ describe('Manage Todos', () => {
       cy.get('[data-test-id=todo-item__checked-checkbox]').should('not.be.checked');
       cy.get('[data-test-id=todo-item__text-input]').should('have.value', itemText);
       cy.get('[data-test-id=todo-item__remove-action]').should('be.visible');
+    });
+  });
+
+  context('Deletion', () => {
+    it('Delete one todo', () => {
+      cy.fixture('todos-list.response.json').then((todoItem) => {
+        let id = todoItem[0]['id'];
+        cy.intercept(
+            'DELETE',
+            todosEndpoint,
+            req => {
+              const {body} = req;
+              expect(body.id).to.equal(id);
+              req.reply();
+            }
+        ).as('deleteTodo');
+      });
+
+      authorize('todos-list.response.json');
+
+      cy.get('[data-test-id=todos-list]').should('be.visible');
+      cy.get('[data-test-id=todo-item]').should('be.visible');
+      cy.get('[data-test-id=task-counter]').should('have.text', "1 item left");
+
+      cy.get('[data-test-id=todo-item__remove-action]').click({force: true})
+      cy.wait('@deleteTodo');
+      cy.get('[data-test-id=todo-item]').should('not.exist');
+      cy.get('[data-test-id=todos-list]').should('be.empty');
+      cy.get('[data-test-id=task-counter]').should('have.text', "0 items left");
     });
   });
 });
